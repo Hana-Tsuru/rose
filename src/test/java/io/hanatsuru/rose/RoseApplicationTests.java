9ee04c12547c3c4ce52669d507f0378cc134ec9f
Alexandru-Ionut.Balan <alexandru-ionut.balan@ing.com>
2022-11-22 17:41:07 +0200

    Initial commit

---
+++ src/test/java/io/hanatsuru/rose/RoseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hanatsuru.rose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
