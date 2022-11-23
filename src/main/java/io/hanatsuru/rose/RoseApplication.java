f5d4b8687a50f99d0a606ed94ff19359d9ff5b2c
Alexandru Ionut Balan <alexandru.ionut.balan97@gmail.com>
2022-11-23 11:41:08 +0200

    Enable reactive mongo repositories

--- src/main/java/io/hanatsuru/rose/RoseApplication.java
+++ src/main/java/io/hanatsuru/rose/RoseApplication.java
@@ -2,8 +2,10 @@ package io.hanatsuru.rose;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class RoseApplication {
 
     public static void main(String[] args) {
